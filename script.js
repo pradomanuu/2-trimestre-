16f512cce945283ce4ec3690ebb01826bee38cd6
pradomanuu <168730277+pradomanuu@users.noreply.github.com>
2024-05-28 10:12:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
+const caixaprincipal=document.queryseletor(".caixa-principal");
+cont caixaperguntas=document.queryseletor(".caixa-pergunta");
+cont caixaalternativa=document.queryseletor("caixa-alternativas");
+const caixaresultado=document.query.seletor("caixa-resultado");
+const textoresultado=document.query.seletor("texto-resultados");
+
+cont perguntas=[
+  {
+    enunciado:"açai ou sorvete?"
+      alternativas:[
+         "açai",
+          "sorvete",
+  ]
+},
+  {
+    enunciado:"azul ou vermelho?"
+      alternativas:[
+         "azul",
+          "vermelho",
+  ]
+},
+  {
+    enunciado:"Frio ou calor?"
+      alternativas:[
+         "FRio",
+          "calor",
+  ]
+},
+  {
+    enunciado:"outono ou primavera?"
+      alternativas:[
+         "outono",
+          "primavera",
+  ]
+},
+  {
+    enunciado:"série ou filme?"
+      alternativas:[
+         "filme",
+          "série",
+  ]
+},
+];
+
+
 
